92fb922b23521944f5505f893ecfff7d10a29ae4
Gonçalo Oliveira <63232629+goncalo-mendes@users.noreply.github.com>
2021-05-21 17:36:58 +0100

    Add files via upload

---
+++ BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookAPI.Modules;
+using BookAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")] //o nome será algo como localhost:8000/api/books
+    [ApiController]
+    public class BooksController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository booksRpository)
+        {
+            _bookRepository = booksRpository;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }
+        [HttpGet("{id}")] //get pelo id
+        public async Task<ActionResult<Book>> GetBooks(int id)
+        {
+            return await _bookRepository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBooks([FromBody] Book book)
+        {
+            var newBook = await _bookRepository.Create(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.id }, newBook);
+        }
+    }
+
+}
---
+++ BookAPI/Modules/BookContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookAPI.Modules;
+
+namespace BookAPI.Modules
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            :base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
